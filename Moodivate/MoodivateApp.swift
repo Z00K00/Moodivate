f58c129dea89f71f85a37a2256513455121e2879
Kraig Larson <147437185+Z00K00@users.noreply.github.com>
2024-03-01 14:53:57 -0500

    Initial Commit

---
+++ Moodivate/MoodivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodivateApp.swift
+//  Moodivate
+//
+//  Created by Krajg Larson on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
